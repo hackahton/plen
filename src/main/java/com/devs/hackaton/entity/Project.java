e0e61f15ac9aa4d908ef145f440f5dcc2aa4be05
dev16 <dbazilio01@gmail.com>
2025-11-07 19:58:10 -0300

    implementação do project

---
+++ src/main/java/com/devs/hackaton/entity/Project.java
@@ -0,0 +1,45 @@
+package com.devs.hackaton.entity;
+
+import com.devs.hackaton.enums.Priority;
+import com.devs.hackaton.enums.ProjectStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "projects")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "project_status")
+    private ProjectStatus projectStatus;
+
+
+    @ManyToMany(mappedBy = "projects")
+    @Column(name = "company_id")
+    private List<Company> companyIds;
+
+    @ManyToMany
+    @JoinTable(name = "project_user",joinColumns = @JoinColumn(name = "project_id"),inverseJoinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "user_id")
+    private List<User> userIds;
+}
