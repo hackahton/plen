3a3a610ff651e53638a2f98f319b3dcc11f2e58a
Vitor <vi.snascimento07@gmail.com>
2025-11-07 19:39:14 -0300

    Feat - Repositorys @

--- src/main/java/com/devs/hackaton/repository/TaskRepository.java
+++ src/main/java/com/devs/hackaton/repository/TaskRepository.java
@@ -2,8 +2,10 @@ package com.devs.hackaton.repository;
 import com.devs.hackaton.entity.Task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface TaskRepository  extends JpaRepository<Task, UUID> {
 }
