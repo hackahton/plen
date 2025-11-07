8f3f0a97c0d6451bfb497e4db5e4c6d3fbb0f98d
Pedro Eduardo <pedro.edu2167@gmail.com>
2025-11-07 19:24:51 -0300

    Remove Report entity and update User associations
    Deleted the Report entity class and added many-to-many relationships for projects and tasks in the User entity. Also updated build.gradle to include validation starter and adjusted dependency order.

--- src/main/java/com/devs/hackaton/entity/User.java
+++ src/main/java/com/devs/hackaton/entity/User.java
@@ -5,6 +5,8 @@ import com.devs.hackaton.enums.Company_User_Status;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 
@@ -18,10 +20,28 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column
     private String name;
     private String email;
     private String password;
     private String cpf;
     private Role role;
     private Company_User_Status status;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_projects",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "project_id")
+    )
+    private List<Project> projects = new ArrayList<>();
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_tasks",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "task_id")
+    )
+    private List<Task> tasks = new ArrayList<>();
 }
