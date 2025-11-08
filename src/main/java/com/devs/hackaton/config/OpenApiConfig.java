a7ed2924181957750267fe31518404441cf8dd82
Pedro Eduardo <pedro.edu2167@gmail.com>
2025-11-08 00:38:54 -0300

    Add OpenAPI config and integrate Swagger UI
    Introduced OpenApiConfig to configure OpenAPI documentation and added springdoc-openapi dependency for Swagger UI support. Also updated UserController to use @RequestBody for user creation endpoint. Downgraded Spring Boot and dependency management plugin versions for compatibility.

---
+++ src/main/java/com/devs/hackaton/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.devs.hackaton.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API")
+                        .version("0.0.1")
+                        .description("Its Api with RabbitMQ ")
+                        .contact(new Contact()
+                                .name("Bank")
+                                .email("pedro.edu2168@gmail.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")));
+    }
+}
--- src/main/java/com/devs/hackaton/controller/UserController.java
+++ src/main/java/com/devs/hackaton/controller/UserController.java
@@ -5,6 +5,7 @@ import com.devs.hackaton.dto.UserDTOs.response.UserResponse;
 import com.devs.hackaton.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserResponse> createUser(UserRequest request){
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest request){
         return ResponseEntity.ok(userService.createUser(request));
     }
 }
