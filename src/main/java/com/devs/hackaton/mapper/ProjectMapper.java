7eea05e9acc81d2288bbb40c5e12ec600dc7e343
dev16 <dbazilio01@gmail.com>
2025-11-08 02:12:17 -0300

    implementação de novas regras de negocio

--- src/main/java/com/devs/hackaton/mapper/ProjectMapper.java
+++ src/main/java/com/devs/hackaton/mapper/ProjectMapper.java
@@ -4,17 +4,22 @@ import com.devs.hackaton.dto.Project.request.CreateProjectRequest;
 import com.devs.hackaton.dto.Project.response.CreateProjectResponse;
 import com.devs.hackaton.entity.Project;
 
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 public class ProjectMapper {
+
     public static Project toProjectEntity(CreateProjectRequest request) {
         return Project.builder()
                 .name(request.name())
                 .description(request.description())
                 .priority(request.priority())
                 .projectStatus(request.projectStatus())
-                .companies(request.companies())
-                .users(request.users())
+                .companies(Collections.emptyList())
+                .users(Collections.emptyList())
                 .build();
     }
+
     public static CreateProjectResponse toResponse(Project project){
         return CreateProjectResponse.builder()
                 .id(project.getId())
@@ -22,8 +27,12 @@ public class ProjectMapper {
                 .description(project.getDescription())
                 .priority(project.getPriority())
                 .projectStatus(project.getProjectStatus())
-                .companies(project.getCompanies())
-                .users(project.getUsers())
+                .companies(project.getCompanies() != null ? project.getCompanies().stream()
+                        .map(CompanyMapper::toCreateCompanyResponse)
+                        .collect(Collectors.toList()) : Collections.emptyList())
+                .users(project.getUsers() != null ? project.getUsers().stream()
+                        .map(UserMapper::toResponse)
+                        .collect(Collectors.toList()) : Collections.emptyList())
                 .build();
     }
 
--- src/main/java/com/devs/hackaton/service/ProjectService.java
+++ src/main/java/com/devs/hackaton/service/ProjectService.java
@@ -1,64 +1,94 @@
 package com.devs.hackaton.service;
 
+import com.devs.hackaton.dto.Project.request.CreateProjectRequest;
+import com.devs.hackaton.dto.Project.response.CreateProjectResponse;
+import com.devs.hackaton.entity.Company;
 import com.devs.hackaton.entity.Project;
 import com.devs.hackaton.entity.User;
 import com.devs.hackaton.enums.Role;
+import com.devs.hackaton.mapper.ProjectMapper;
+import com.devs.hackaton.repository.CompanyRepository; // Novo Import
 import com.devs.hackaton.repository.ProjectRepository;
 import com.devs.hackaton.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class ProjectService {
 
     private final ProjectRepository projectRepository;
-    private final UserRepository userRepository; // Adicionado para validar associações
+    private final UserRepository userRepository;
+    private final CompanyRepository companyRepository; // Adicionado
 
     @Autowired
-    public ProjectService(ProjectRepository projectRepository, UserRepository userRepository) {
+    public ProjectService(ProjectRepository projectRepository, UserRepository userRepository, CompanyRepository companyRepository) {
         this.projectRepository = projectRepository;
         this.userRepository = userRepository;
+        this.companyRepository = companyRepository;
     }
 
-    public Project saveProject(Project project, Role userRole) {
+    @Transactional
+    public CreateProjectResponse createProject(CreateProjectRequest request, Role userRole) {
+        if (userRole == Role.COLABORADOR) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+                    "Colaboradores não podem criar novos projetos. Somente GESTOR ou SUPERVISOR.");
+        }
+        if (request.companyId() == null || request.userId() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "ID da Empresa e do Usuário são obrigatórios para criação do projeto.");
+        }
 
-        if (project.getId() == null) {
+        Project project = ProjectMapper.toProjectEntity(request);
+
+        Company company = companyRepository.findById(request.companyId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Empresa (ID: " + request.companyId() + ") não encontrada."));
+
+        User user = userRepository.findById(request.userId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário inicial (ID: " + request.userId() + ") não encontrado."));
+
+        project.getCompanies().add(company);
+        company.getProjects().add(project);
+        project.getUsers().add(user);
+        user.getProjects().add(project);
+
+        project = projectRepository.save(project);
+
+        return ProjectMapper.toResponse(project);
+    }
+
+    @Transactional
+    public CreateProjectResponse updateProject(UUID projectId, CreateProjectRequest request, Role userRole) {
+
+        Project existingProject = projectRepository.findById(projectId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Projeto com ID " + projectId + " não encontrado."));
+
+        if (existingProject.getProjectStatus() != request.projectStatus()) {
             if (userRole == Role.COLABORADOR) {
                 throw new ResponseStatusException(HttpStatus.FORBIDDEN,
-                        "Colaboradores não podem criar novos projetos. Somente GESTOR ou SUPERVISOR.");
+                        "Usuário com função " + userRole + " não tem permissão para alterar o status do projeto.");
             }
-            return projectRepository.save(project);
         }
 
 
-        Optional<Project> existingProjectOpt = projectRepository.findById(project.getId());
-
-        if (existingProjectOpt.isPresent()) {
-            Project existingProject = existingProjectOpt.get();
-
-            if (existingProject.getProjectStatus() != project.getProjectStatus()) {
+        existingProject.setName(request.name());
+        existingProject.setDescription(request.description());
+        existingProject.setPriority(request.priority());
+        existingProject.setProjectStatus(request.projectStatus());
 
 
-                if (userRole == Role.COLABORADOR) {
-                    throw new ResponseStatusException(HttpStatus.FORBIDDEN,
-                            "Usuário com função " + userRole + " não tem permissão para alterar o status do projeto.");
-                }
-            }
-
-            return projectRepository.save(project);
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-                    "Projeto com ID " + project.getId() + " não encontrado.");
-        }
+        return ProjectMapper.toResponse(projectRepository.save(existingProject));
     }
 
-    public Project associateUser(UUID projectId, UUID userId, Role userRole) {
+
+    @Transactional
+    public CreateProjectResponse associateUser(UUID projectId, UUID userId, Role userRole) {
         if (userRole == Role.COLABORADOR) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN,
                     "Somente GESTOR ou SUPERVISOR podem associar usuários a um projeto.");
@@ -77,18 +107,23 @@ public class ProjectService {
         project.getUsers().add(user);
         user.getProjects().add(project);
 
-        return projectRepository.save(project);
+        return ProjectMapper.toResponse(projectRepository.save(project));
     }
 
-    public List<Project> findAllProjects() {
-        return projectRepository.findAll();
+    public List<CreateProjectResponse> findAllProjects() {
+        return projectRepository.findAll().stream()
+                .map(ProjectMapper::toResponse)
+                .collect(Collectors.toList());
     }
 
-    public Optional<Project> findProjectById(UUID id) {
-        return projectRepository.findById(id);
+    public Optional<CreateProjectResponse> findProjectById(UUID id) {
+        return projectRepository.findById(id)
+                .map(ProjectMapper::toResponse);
     }
 
+    @Transactional
     public void deleteProject(UUID id) {
         projectRepository.deleteById(id);
     }
+
 }
