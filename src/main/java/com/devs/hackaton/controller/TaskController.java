db4ba580385dbf06e86c052f1b745062234a938c
dev16 <dbazilio01@gmail.com>
2025-11-08 05:43:18 -0300

    implementação de novas regras de negocio

--- src/main/java/com/devs/hackaton/controller/TaskController.java
+++ src/main/java/com/devs/hackaton/controller/TaskController.java
@@ -1,9 +1,12 @@
 package com.devs.hackaton.controller;
 
+import com.devs.hackaton.dto.Task.request.AtribuicaoRequest;
+import com.devs.hackaton.dto.Task.request.MudancaRequest;
 import com.devs.hackaton.dto.Task.request.TaskRequest;
 import com.devs.hackaton.dto.Task.response.TaskResponse;
 import com.devs.hackaton.service.TaskService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,17 +21,20 @@ public class TaskController {
     private final TaskService taskService;
 
     @PostMapping
-    public ResponseEntity<TaskResponse> createTask(TaskRequest request) {
-        return ResponseEntity.ok(taskService.criarTask(request));
+    public ResponseEntity<?> createTask(TaskRequest request) {
+        taskService.criarTask(request);
+        return ResponseEntity.status(HttpStatus.OK).body("Criado");
     }
 
     @PutMapping("/editar")
-    public ResponseEntity<TaskResponse> editTask(TaskRequest request) {
-        return ResponseEntity.ok(taskService.editarTask(request));
+    public ResponseEntity<?> editTask(MudancaRequest request) {
+        taskService.editarTask(request);
+        return ResponseEntity.status(HttpStatus.OK).body("Editado");
     }
 
     @PutMapping("/atribuicao")
-    public ResponseEntity<TaskResponse> atribuicaoTask(TaskRequest request) {
-        return ResponseEntity.ok(taskService.atribuirTask(request);
+    public ResponseEntity<?> atribuicaoTask(AtribuicaoRequest request) {
+        taskService.atribuirTask(request);
+        return ResponseEntity.status(HttpStatus.OK).body("Atribuido");
     }
 }
