f0fb73796a5e6f051a74f6676d15de7b0fb2ee6e
Vitor <vi.snascimento07@gmail.com>
2025-11-07 21:27:47 -0300

    Fix - UsarService

--- src/main/java/com/devs/hackaton/service/UserService.java
+++ src/main/java/com/devs/hackaton/service/UserService.java
@@ -1,8 +1,6 @@
 package com.devs.hackaton.service;
 
-import com.devs.hackaton.entity.Project;
 import com.devs.hackaton.entity.User;
-import com.devs.hackaton.repository.ProjectRepository;
 import com.devs.hackaton.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
